6fe85f56eef033388f65006ece22c8e18a3ecef7
srinjaysaha <59783253+srinjaysaha@users.noreply.github.com>
2020-10-02 18:49:18 +0530

    Add files via upload

---
+++ Pocket Nurse covid19/public/js/home.js
@@ -0,0 +1,9 @@
+function takeToQuiz(e){
+    e.preventDefault();
+    window.location = "quiz/JavaScript QUiz/start.html";
+}
+
+function takeToBedCount(e){
+    e.preventDefault();
+    window.location = "HospitalBed.html";
+}
